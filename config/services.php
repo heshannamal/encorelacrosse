20bdb7862f8b5d0d1014308f8415b4c7d2f7c743
heshannamal <159673750+heshannamal@users.noreply.github.com>
2026-09-17 16:55:49 +0530

    Configure Encore storefront services

--- config/services.php
+++ config/services.php
@@ -1,18 +1,9 @@
 <?php
 
-return [
+$encoreApiUrl = rtrim((string) env('ENCORE_API_URL', 'https://inventory.encr.co/api'), '/');
+$encoreAssetUrl = rtrim((string) env('ENCORE_ASSET_URL', 'https://encr.co'), '/');
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+return [
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
@@ -30,9 +21,28 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+    'encore' => [
+        'base_url' => $encoreApiUrl,
+        'api_url' => $encoreApiUrl,
+        'asset_url' => $encoreAssetUrl,
+        'api_key' => env('ENCORE_API_KEY', 'inventory_products_api'),
+        'api_token' => env('ENCORE_API_TOKEN'),
+        'sales_channel_id' => env('ENCORE_SALES_CHANNEL_ID', 1),
+        'inventory_type_id' => env('ENCORE_INVENTORY_TYPE_ID', 2),
+        'currency_symbol' => env('ENCORE_CURRENCY_SYMBOL', '$'),
+        'timeout' => env('ENCORE_API_TIMEOUT', 30),
+        'connect_timeout' => env('ENCORE_API_CONNECT_TIMEOUT', 10),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
 ];
