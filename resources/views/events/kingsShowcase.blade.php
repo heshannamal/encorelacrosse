655e50258c5c07427e5cce231b657473b07c012c
heshannamal <159673750+heshannamal@users.noreply.github.com>
2026-09-26 01:21:43 +0530

    Use lowercase JPG paths for Linux server

--- resources/views/events/kingsShowcase.blade.php
+++ resources/views/events/kingsShowcase.blade.php
@@ -7,7 +7,7 @@
 $heroBgImage = asset('images/event/kingsShowcase/backgroundImage.jpg');
 
 // --- Hotels ---
-$suncoastImg = asset('images/event/kingsShowcase/image1.JPG');
+$suncoastImg = asset('images/event/kingsShowcase/image1.jpg');
 $bestWesternImg = asset('images/event/kingsShowcase/image2.avif');
 $rodewayInnImg = asset('images/event/kingsShowcase/image3.avif');
 $cirqueImg1 = asset('images/event/kingsShowcase/image4.jpg');
