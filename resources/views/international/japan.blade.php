d38a872b1c783f22289bf5b5a94a5ce3ae132bcc
heshannamal <159673750+heshannamal@users.noreply.github.com>
2026-09-26 01:21:58 +0530

    Use lowercase JPG paths for Linux server

--- resources/views/international/japan.blade.php
+++ resources/views/international/japan.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 @php
-$heroBgImage = asset('images/international/japan/backgroundImage.JPG');
-$image1 = asset('images/international/japan/image1.JPG');
+$heroBgImage = asset('images/international/japan/backgroundImage.jpg');
+$image1 = asset('images/international/japan/image1.jpg');
 $image2 = asset('images/international/japan/image2.jpg');
 @endphp
 
